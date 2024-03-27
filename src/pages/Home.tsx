12dea88d1e7571b91491f177088ceacda0ca4b0c
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-03-27 16:32:12 -0700

    created footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,22 +62,25 @@ function App() {
         />
         <h2 className="banner-text">Create Unforgettable Memories</h2>
       </section>
-      <section className="social-preview">
-        <img
-          className="banner-img"
-          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-          alt="Field of grass during sunset."
-        />
-        <img
-          className="banner-img"
-          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-          alt="Field of grass during sunset."
-        />
-        <img
-          className="banner-img"
-          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-          alt="Field of grass during sunset."
-        />
+      <section className="footer">
+        <h2> Catch us on instagram</h2>
+        <div className="social-posts-footer">
+          <img
+            className="social-preview-img"
+            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+            alt="Field of grass during sunset."
+          />
+          <img
+            className="social-preview-img"
+            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+            alt="Field of grass during sunset."
+          />
+          <img
+            className="social-preview-img"
+            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+            alt="Field of grass during sunset."
+          />
+        </div>
       </section>
     </main>
   );
