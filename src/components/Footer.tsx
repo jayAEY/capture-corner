da043f414b0eeaf3eae08fe57e60b222bbe6ed67
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-03-28 16:38:22 -0700

    moved footer to its own component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2> Catch us on instagram</h2>
+      <div className="social-posts-footer">
+        <img
+          className="social-preview-img"
+          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+          alt="Field of grass during sunset."
+        />
+        <img
+          className="social-preview-img"
+          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+          alt="Field of grass during sunset."
+        />
+        <img
+          className="social-preview-img"
+          src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
+          alt="Field of grass during sunset."
+        />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import "../App.scss";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
+import Footer from "../components/Footer";
 
 function App() {
   return (
@@ -62,26 +63,7 @@ function App() {
         />
         <h2 className="banner-text">Create Unforgettable Memories</h2>
       </section>
-      <section className="footer">
-        <h2> Catch us on instagram</h2>
-        <div className="social-posts-footer">
-          <img
-            className="social-preview-img"
-            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-            alt="Field of grass during sunset."
-          />
-          <img
-            className="social-preview-img"
-            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-            alt="Field of grass during sunset."
-          />
-          <img
-            className="social-preview-img"
-            src="photos/benjamin-davies-Zm2n2O7Fph4-unsplash.jpg"
-            alt="Field of grass during sunset."
-          />
-        </div>
-      </section>
+      <Footer />
     </main>
   );
 }
