4aa73d330a43543e91d6988bfc1db8f21a57074d
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-04-02 16:47:20 -0700

    finished contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,76 @@
 import React from "react";
 
 const contact = () => {
-  return <div>contact</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    let form = document.querySelector("#contact-form");
+    let submitSection = document.querySelector("#submit-success");
+    form.style.display = "none";
+    submitSection.style.display = "flex";
+  }
+  return (
+    <main id="contact-page">
+      <form
+        id="contact-form"
+        className="contact-main"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <h1 className="h1-first-line">contact us</h1>
+        <h1 className="h1-second-line">say hi!</h1>
+        <h2>please fill our form below and we'll be in touch soon!</h2>
+        <input
+          type="text"
+          form="contact-form"
+          name="name"
+          placeholder="your name"
+        />
+        <input
+          type="email"
+          form="contact-form"
+          name="email"
+          placeholder="email"
+          required
+        />
+        <select
+          name="package"
+          id="package"
+          form="contact-form"
+        >
+          <option defaultValue="select">select package</option>
+          <option value="babies">babies / newborns</option>
+          <option value="families">families</option>
+          <option value="children">children</option>
+          <option value="other">other</option>
+        </select>
+        <input
+          type="text"
+          form="contact-form"
+          name="location"
+          placeholder="preferred location"
+        />
+        <input
+          id="text-area"
+          type="text-area"
+          name="message"
+          placeholder="tell us more!"
+        ></input>
+        <button
+          id="submit"
+          type="submit"
+        >
+          send away!
+        </button>
+      </form>
+      <section
+        id="submit-success"
+        className="contact-main"
+      >
+        <h1 className="h1-first-line">Form Sent</h1>
+        <h1 className="h1-second-line">Thank You!</h1>
+        <h2> We'll be in touch soon!</h2>
+      </section>
+    </main>
+  );
 };
 
 export default contact;
