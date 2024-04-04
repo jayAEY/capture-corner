0bd2f4c1625a3c4669e6de18821abbfe1ee36b07
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-04-04 16:21:40 -0700

    continued desktop version updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,7 +44,16 @@ const Footer = () => {
         </section>
       </footer>
       <footer id="desktop-footer">
-        <section className="social-posts-footer-section">
+        <section className="footer-area">
+          <h2 className="footer-area-text">
+            <Link to="/meet">
+              A little bit more
+              <FaArrowRight size={"30px"} />
+            </Link>
+          </h2>
+          <h3 className="banner-h3">About us</h3>
+        </section>
+        <section className="footer-social-area">
           <h3> Catch us on instagram</h3>
           <div className="social-posts-footer">
             <a href="https://instagram.com">
@@ -70,19 +79,14 @@ const Footer = () => {
             </a>
           </div>
         </section>
-        {/* <section className="banner"> */}
-        {/* <img
-          className="banner-img"
-          src="photos/mark-harpur-K2s_YE031CA-unsplash.jpg"
-          alt="Field of flowers during sunset."
-        /> */}
-        <h2 className="banner-text">
-          <Link to="/contact">
-            Ready to get in touch? <FaArrowRight size={"30px"} />
-          </Link>
-        </h2>
-        <h3 className="banner-h3">Right this way</h3>
-        {/* </section> */}
+        <section className="footer-area">
+          <h2 className="footer-area-text">
+            <Link to="/contact">
+              Ready to get in touch? <FaArrowRight size={"30px"} />
+            </Link>
+          </h2>
+          <h3 className="banner-h3">Right this way</h3>
+        </section>
       </footer>
     </>
   );
